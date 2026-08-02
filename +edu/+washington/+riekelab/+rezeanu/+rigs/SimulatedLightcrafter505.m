ea868c156e947f507dd094fa6b69729ba758075c
Dragos Rezeanu <DRezeanu@uw.edu>
2026-08-01 23:26:35 -0700

    Moved main directory, created utilities and rig config

---
+++ +edu/+washington/+riekelab/+rezeanu/+rigs/SimulatedLightcrafter505.m
@@ -0,0 +1,114 @@
+classdef SimulatedLightcrafter505 < symphonyui.core.descriptions.RigDescription
+    
+    methods
+        
+        function obj = SimulatedLightcrafter505()
+            import symphonyui.builtin.daqs.*;
+            import symphonyui.builtin.devices.*;
+            import symphonyui.core.*;
+            
+            daq = HekaSimulationDaqController();
+            obj.daqController = daq;
+            
+            % Rig name and laboratory.
+            rigDev = manookinlab.devices.RigPropertyDevice('RiekeLab','SimulatedLightcrafter');
+            obj.addDevice(rigDev);
+            
+            amp1 = MultiClampDevice('Amp1', 1).bindStream(daq.getStream('ao0')).bindStream(daq.getStream('ai0'));
+            obj.addDevice(amp1);
+
+            % Add an analog trigger device to simulate the MEA.
+            trigger = UnitConvertingDevice('ExternalTrigger', 'V').bindStream(daq.getStream('ao1'));
+            obj.addDevice(trigger);
+           
+            % The 505 nm LED.
+            greenRamp = importdata(edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_gamma_ramp.txt'));
+            green = CalibratedDevice('Green LED', Measurement.NORMALIZED, greenRamp(:, 1), greenRamp(:, 2)).bindStream(daq.getStream('ao2'));
+            green.addConfigurationSetting('ndfs', {}, ...
+                'type', PropertyType('cellstr', 'row', {'C1', 'C2', 'C3', 'C4', 'C5'}));
+            green.addResource('ndfAttenuations', containers.Map( ...
+                {'C1', 'C2', 'C3', 'C4', 'C5'}, ...
+                {0.2866, 0.5933, 0.9675, 1.9279, 2.1372}));
+            green.addConfigurationSetting('gain', '', ...
+                'type', PropertyType('char', 'row', {'', 'low', 'medium', 'high'}));
+            green.addResource('fluxFactorPaths', containers.Map( ...
+                {'low', 'medium', 'high'}, { ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_low_flux_factors.txt'), ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_medium_flux_factors.txt'), ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_high_flux_factors.txt')}));
+            green.addConfigurationSetting('lightPath', 'below', 'isReadOnly', true);
+            green.addResource('spectrum', importdata(edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'green_led_spectrum.txt')));
+            obj.addDevice(green);
+            
+            
+%              trigger1 = UnitConvertingDevice('Trigger1', symphonyui.core.Measurement.UNITLESS).bindStream(daq.getStream('doport1'));
+%             daq.getStream('doport1').setBitPosition(trigger1, 0);
+%             obj.addDevice(trigger1);
+%             
+%             trigger2 = UnitConvertingDevice('Trigger2', symphonyui.core.Measurement.UNITLESS).bindStream(daq.getStream('doport1'));
+%             daq.getStream('doport1').setBitPosition(trigger2, 2);
+%             obj.addDevice(trigger2);
+            
+            frameMonitor = UnitConvertingDevice('Frame Monitor', 'V').bindStream(obj.daqController.getStream('ai7'));
+            obj.addDevice(frameMonitor);
+            
+            microdisplay = edu.washington.riekelab.rezeanu.devices.SimulatedLcrVideoMode('micronsPerPixel', 3.07);
+            
+            microdisplay.addResource('fluxFactorPaths', containers.Map( ...
+                {'auto', 'red', 'green', 'blue'}, { ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_auto505_flux_factors.txt'), ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_red505_flux_factors.txt'), ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_green505_flux_factors.txt'), ...
+                edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_blue505_flux_factors.txt')}));
+            microdisplay.addConfigurationSetting('lightPath', 'below', 'isReadOnly', true);
+            
+            myspect = containers.Map( ...
+                {'auto', 'red', 'green', 'blue'}, { ...
+                importdata(edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_auto505_spectrum.txt')), ...
+                importdata(edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_red505_spectrum.txt')), ...
+                importdata(edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_green505_spectrum.txt')), ...
+                importdata(edu.washington.riekelab.Package.getCalibrationResource('rigs', 'suction', 'lightcrafter_below_blue505_spectrum.txt'))});
+            
+            microdisplay.addResource('spectrum', myspect);
+
+            
+            
+            % Compute the Quantal catch.
+            qCatch = [
+               0.664987   0.169773   0.040604   0.154258
+               0.638458   1.136154   0.227892   3.911955
+               0.114495   0.115746   1.121788   0.715405]*1e6;
+           
+            microdisplay.addResource('quantalCatch', qCatch);
+            
+            microdisplay.addConfigurationSetting('ndfs', {}, ...
+                'type', PropertyType('cellstr', 'row', {'FW00', 'FW05', 'FW10', 'FW20', 'FW30', 'FW40','C1', 'C2', 'C3', 'C4', 'C5'}));
+            
+            microdisplay.addResource('ndfAttenuations', containers.Map( ...
+                {'auto','red', 'green', 'blue'}, { ...
+                containers.Map( ...
+                    {'FW00', 'FW05', 'FW10', 'FW20', 'FW30', 'FW40','C1', 'C2', 'C3', 'C4', 'C5'}, ...
+                    {0, 0.5153, 1.0175, 2.1873, 3.2603, 4.3110, 0.2866, 0.5460, 0.9675, 1.9279, 2.1372}), ...
+                containers.Map( ...
+                    {'FW00', 'FW05', 'FW10', 'FW20', 'FW30', 'FW40','C1', 'C2', 'C3', 'C4', 'C5'}, ...
+                    {0, 0.5089, 1.0047, 2.0381, 3.0604, 4.0434, 0.2866, 0.5945, 0.9675, 1.9279, 2.1372}), ...
+                containers.Map( ...
+                    {'FW00', 'FW05', 'FW10', 'FW20', 'FW30', 'FW40','C1', 'C2', 'C3', 'C4', 'C5'}, ...
+                    {0, 0.5074, 1.0031, 2.1752, 3.2626, 4.2867, 0.2866, 0.5470, 0.9675, 1.9279, 2.1372}), ...
+                containers.Map( ...
+                    {'FW00', 'FW05', 'FW10', 'FW20', 'FW30', 'FW40','C1', 'C2', 'C3', 'C4', 'C5'}, ...
+                    {0, 0.5312, 1.0494, 2.4151, 3.6192, 4.7103, 0.2663, 0.5140, 0.9569, 2.0810, 2.3747})}));    
+            
+            % Binding the video device to an unused stream only so its configuration settings are written to each epoch.
+            microdisplay.bindStream(daq.getStream('doport1'));
+            daq.getStream('doport1').setBitPosition(microdisplay, 15);    
+            obj.addDevice(microdisplay);
+            
+            % Add the filter wheel.
+%             filterWheel = manookinlab.devices.FilterWheelDevice('comPort', 'COM13');
+%             obj.addDevice(filterWheel);
+        end
+    end
+    
+end
+
---
+++ +edu/+washington/+riekelab/+rezeanu/+utils/getLcrQuantalCatch.m
@@ -0,0 +1,113 @@
+function qCatch = getLcrQuantalCatch(device, persistor)
+    % Grab epoch group 
+    epoch_group = persistor.currentEpochGroup;
+    
+    if isempty(epoch_group)
+        error('Must have an active epoch group to get quantal catch values')
+    end
+    
+    % Pull spectrum, ndf attenuations, fluxFactors, current active
+    % ndfs and light path from Stage device
+    spectrum = device.getResource('spectrum');
+    attenuations = device.getResource('ndfAttenuations');
+    fluxFactors = device.getResource('fluxFactors');
+    ndfs = device.getConfigurationSetting('ndfs');
+    path = device.getConfigurationSetting('lightPath');
+
+    % Pull channel specific NDF attenuation values
+    r_attenuations = attenuations('red');
+    g_attenuations = attenuations('green');
+    b_attenuations = attenuations('blue');
+
+    % Get Species for Photoreceptor Spectral Sensitivities
+    source = epoch_group.source;
+    while ~isempty(source) && ~any(strcmp(source.getResourceNames(), 'photoreceptors'))
+        source = source.parent;
+    end
+    species = source;
+
+    photoreceptors = species.getResource('photoreceptors');
+
+    % Get Preparation for Photoreceptor Orientation info, which is
+    % used to get collecting area
+    source = epoch_group.source;
+    while ~isempty(source) ...
+            && isempty(source.getPropertyDescriptors().findByName('preparation')) ...
+            && ~any(strcmp(source.getResourceNames(), 'photoreceptorOrientations'))
+        source = source.parent;
+    end
+    preparation = source;
+    prep = preparation.getProperty('preparation');
+
+    pr_orientations = preparation.getResource('photoreceptorOrientations');
+    if pr_orientations.isKey(prep)
+        pr_orientation = pr_orientations(prep);
+    else
+        pr_orientation = '';
+    end
+
+    r_factor = fluxFactors('red');
+    g_factor = fluxFactors('green');
+    b_factor = fluxFactors('blue');
+
+    r_spectrum = spectrum('red');
+    g_spectrum = spectrum('green');
+    b_spectrum = spectrum('blue');
+
+    intensity=1.0;
+    units = 'intensity';
+    
+    lmsToRgb = zeros(3,4);
+    
+    % Swap rods with s cones so the PR names are listed as
+    % L, M, S, rod instead of L, M, rod, S;
+    prNames = photoreceptors.keys;
+    temp = prNames{4};
+    prNames{4}=prNames{3};
+    prNames{3} = temp;
+    
+    for i = 1:length(prNames)
+        pr = prNames{i};
+        collectingArea = getCollectingArea(photoreceptors(pr).collectingArea, ...
+            path, pr_orientation);
+
+        lmsToRgb(1,i) = edu.washington.riekelab.util.convisom(intensity, units, r_factor, r_spectrum, ...
+                photoreceptors(pr).spectrum, collectingArea, ndfs, r_attenuations);
+    end
+    
+    for i = 1:length(prNames)
+        pr = prNames{i};
+        collectingArea = getCollectingArea(photoreceptors(pr).collectingArea, ...
+            path, pr_orientation);
+
+        lmsToRgb(2,i) = edu.washington.riekelab.util.convisom(intensity, units, g_factor, g_spectrum, ...
+                photoreceptors(pr).spectrum, collectingArea, ndfs, g_attenuations);
+    end
+    
+    for i = 1:length(prNames)
+        pr = prNames{i};
+        collectingArea = getCollectingArea(photoreceptors(pr).collectingArea, ...
+            path, pr_orientation);
+
+        lmsToRgb(3,i) = edu.washington.riekelab.util.convisom(intensity, units, b_factor, b_spectrum, ...
+                photoreceptors(pr).spectrum, collectingArea, ndfs, b_attenuations);
+    end
+   
+    qCatch = round(lmsToRgb);
+end
+
+function a = getCollectingArea(map, path, orientation)
+    if (strcmpi(path, 'below') && any(strcmpi(orientation, {'down', 'lateral'}))) ...
+            || (strcmpi(path, 'above') && any(strcmpi(orientation, {'up', 'lateral'})))
+        a = map('photoreceptorSide');
+    elseif (strcmpi(path, 'below') && strcmpi(orientation, 'up')) ...
+            || (strcmpi(path, 'above') && strcmpi(orientation, 'down'))
+        a = map('ganglionCellSide');
+    else
+        warning('Unexpected light path or photoreceptor orientation. Using 0 for collecting area.');
+        a = 0;
+    end
+end
+
+           
+            
